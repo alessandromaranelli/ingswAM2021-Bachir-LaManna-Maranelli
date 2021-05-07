3fc9701f51da877d81f8d20be8301bcc6929f862
AmirBachir <amir.bachir@mail.polimi.it>
2021-05-07 11:39:50 +0200

    production and development buy user input parsing

--- src/main/java/it/polimi/ingsw/client/OutputView.java
+++ src/main/java/it/polimi/ingsw/client/OutputView.java
@@ -1,8 +1,14 @@
 package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.messages.commands.*;
+import it.polimi.ingsw.messages.commands.buydevelopmentphase.BuyCardMsg;
+import it.polimi.ingsw.messages.commands.buydevelopmentphase.BuyDevelopmentPhaseMsg;
+import it.polimi.ingsw.messages.commands.buydevelopmentphase.PayCardFromChestMsg;
+import it.polimi.ingsw.messages.commands.buydevelopmentphase.PayCardFromStorageMsg;
 import it.polimi.ingsw.messages.commands.marketphase.*;
 import it.polimi.ingsw.messages.commands.preparation.*;
+import it.polimi.ingsw.messages.commands.productionphase.*;
+import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Resource;
 import it.polimi.ingsw.model.TurnState;
 
@@ -77,6 +83,39 @@ public class OutputView implements Runnable{
         if(type == TypeOfCommand.ORGANIZERESOURCES){
             return new StartOrganizeResourcesMsg();
         }
+        if(type == TypeOfCommand.SELECTBUYDEVELOPMENTCARDPHASE){
+            return new BuyDevelopmentPhaseMsg();
+        }
+        if(type == TypeOfCommand.BUYDEVELOPMENTCARD){
+            return new BuyCardMsg(Color.valueOf(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+        }
+        if(type == TypeOfCommand.PAYCARDFROMCHEST){
+            return new PayCardFromChestMsg(Resource.valueOf(parts[1]), Integer.parseInt(parts[2]));
+        }
+        if(type == TypeOfCommand.PAYCARDFROMSTORAGE){
+            return new PayCardFromStorageMsg(Resource.valueOf(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+        }
+        if(type == TypeOfCommand.SELECTPRODUCTIONPHASE){
+            return new SelectProductionPhaseMsg();
+        }
+        if(type == TypeOfCommand.ACTIVATEPERSONALPRODUCTION){
+            return new ActivatePersonalProductionMsg(Resource.valueOf(parts[1]), Resource.valueOf(parts[2]), Resource.valueOf(parts[3]));
+        }
+        if(type == TypeOfCommand.ACTIVATEPRODUCTION){
+            return new ActivateProductionMsg(Integer.parseInt(parts[1]));
+        }
+        if(type == TypeOfCommand.ACTIVATESPECIALPRODUCTION){
+            return new ActivateSpecialProduction(Resource.valueOf(parts[1]), Integer.parseInt(parts[2]));
+        }
+        if(type == TypeOfCommand.STARTPAYPRODUCTION){
+            return new StartPayProductionMsg();
+        }
+        if(type == TypeOfCommand.PAYPRODUCTIONFROMSTORAGE){
+            return new PayProductionFromStorage(Resource.valueOf(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+        }
+        if(type == TypeOfCommand.PAYPRODUCTIONFROMCHEST){
+            return new PayProductionFromChest(Resource.valueOf(parts[1]), Integer.parseInt(parts[2]));
+        }
         else return null;
     }
 
@@ -101,6 +140,57 @@ public class OutputView implements Runnable{
             this.type = TypeOfCommand.ORGANIZERESOURCES;
             return true;
         }
+
+
+        if (parts[0].toLowerCase().equals("selectbuydevelopmentcardphase") && client.getLightModel().getPhase() == TurnState.START){
+            this.type = TypeOfCommand.SELECTBUYDEVELOPMENTCARDPHASE;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("buydevelopmentcard") && client.getLightModel().getPhase() == TurnState.BUYDEVELOPMENTCARDPHASE){
+            this.type = TypeOfCommand.BUYDEVELOPMENTCARD;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("paycardfromchest") && client.getLightModel().getPhase() == TurnState.PAYDEVELOPMENTCARD){
+            this.type = TypeOfCommand.PAYCARDFROMCHEST;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("paycardfromstorage") && client.getLightModel().getPhase() == TurnState.PAYDEVELOPMENTCARD){
+            this.type = TypeOfCommand.PAYCARDFROMSTORAGE;
+            return true;
+        }
+
+
+        if (parts[0].toLowerCase().equals("selectproductionphase") && client.getLightModel().getPhase() == TurnState.START){
+            this.type = TypeOfCommand.SELECTPRODUCTIONPHASE;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("activatepersonalproduction") && client.getLightModel().getPhase() == TurnState.PRODUCTIONPHASE){
+            this.type = TypeOfCommand.ACTIVATEPERSONALPRODUCTION;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("activateproduction") && client.getLightModel().getPhase() == TurnState.PRODUCTIONPHASE){
+            this.type = TypeOfCommand.ACTIVATEPRODUCTION;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("activatespecialproduction") && client.getLightModel().getPhase() == TurnState.PRODUCTIONPHASE){
+            this.type = TypeOfCommand.ACTIVATESPECIALPRODUCTION;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("startpayproduction") && client.getLightModel().getPhase() == TurnState.PRODUCTIONPHASE){
+            this.type = TypeOfCommand.STARTPAYPRODUCTION;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("payproductionfromchest") && client.getLightModel().getPhase() == TurnState.PAYPRODUCTIONS){
+            this.type = TypeOfCommand.PAYPRODUCTIONFROMCHEST;
+            return true;
+        }
+        if (parts[0].toLowerCase().equals("payproductionfromstorage") && client.getLightModel().getPhase() == TurnState.PAYPRODUCTIONS){
+            this.type = TypeOfCommand.PAYPRODUCTIONFROMSTORAGE;
+            return true;
+        }
+
+
+
         else return false;
     }
 }
