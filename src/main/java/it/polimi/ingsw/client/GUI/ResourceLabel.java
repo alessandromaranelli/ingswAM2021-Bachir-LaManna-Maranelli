e26479590457dccb3fff650834d7471f85b8a6d5
AmirBachir <amir.bachir@mail.polimi.it>
2021-06-02 22:47:56 +0200

    pay devel cards frame, resourcelabel (resource img fix)

--- src/main/java/it/polimi/ingsw/client/GUI/ResourceLabel.java
+++ src/main/java/it/polimi/ingsw/client/GUI/ResourceLabel.java
@@ -6,31 +6,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ResourceLabel extends JLabel {
-    JLabel j;
     public ResourceLabel(Resource r){
-        j = new JLabel();
         ImageIcon img;
         switch(r){
             case SERVANT:
-                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/servant.png").getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/servant.png").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
                 break;
             case SHIELD:
-                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/shield.png").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/shield.png").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
                 break;
             case STONE:
-                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/stone.png").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/stone.png").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
                 break;
             default:
-                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/coin.png").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+                img = new ImageIcon(new ImageIcon("src/main/resources/Resources/coin.png").getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
         }
-        j.setHorizontalAlignment(CENTER);
-        j.setVerticalAlignment(TOP);
-        j.setIcon(img);
-        j.setVisible(true);
+        setHorizontalAlignment(CENTER);
+        setVerticalAlignment(TOP);
+        setIcon(img);
+        setVisible(true);
     }
     public void setQuantity(int i){
-        j.setText("" + i);
-        j.setVerticalTextPosition(JLabel.BOTTOM);
-        j.setHorizontalTextPosition(JLabel.CENTER);
+        setText("" + i);
+        setVerticalTextPosition(JLabel.BOTTOM);
+        setHorizontalTextPosition(JLabel.CENTER);
     }
 }
